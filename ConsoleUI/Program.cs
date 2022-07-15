6fe36d0c759729aecae4356ac459b44d7a9a33ff
Batuhan Erol <bataer57@gmail.com>
2022-07-15 16:35:57 +0300

    Updated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Business.Concrete;
 using DataAccess.Concrete.InMemory;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 
 namespace ConsoleUI
@@ -9,21 +10,18 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            carManager.Add(new Car { BrandId = 1, ColorId = 2, DailyPrice = 354, Description = "CAR4", Id = 4, ModelYear = 1935 });
-            Car car5 = new Car { BrandId = 1, ColorId = 3, DailyPrice = 354, Description = "CAR5", Id = 5, ModelYear = 1935 };
-            carManager.Add(car5);
-            carManager.Update(new Car { BrandId = 3, ColorId = 4, DailyPrice = 554, Description = "CAR5 But Updated", Id = 5, ModelYear = 1935 });
-            foreach (var item in carManager.GetById(5))
+            CarManager carManager = new CarManager(new EfCarDal());
+            Car car = new Car
             {
-                Console.WriteLine(item.Description);
-            }
+                BrandId = 1,
+                ColorId = 1,
+                Id = 1,
+                Name = "BD",
+                DailyPrice = 1,
+                ModelYear = 1
+            };
+            carManager.Add(car);
 
-            carManager.Delete(new Car { BrandId = 3, ColorId = 4, DailyPrice = 554, Description = "CAR5 But Updated", Id = 5, ModelYear = 1935 });
-            foreach (var item in carManager.GetAll())
-            {
-                Console.WriteLine(item.Id);
-            }
         }
     }
 }
