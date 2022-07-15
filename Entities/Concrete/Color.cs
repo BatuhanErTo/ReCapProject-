02845c7c342631a1d0dd025320a249d76577bb25
Batuhan Erol <bataer57@gmail.com>
2022-07-15 13:52:18 +0300

    Updated

--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Color
+    public class Color : IEntities
     {
         public int Id { get; set; }
         public string Name { get; set; }
