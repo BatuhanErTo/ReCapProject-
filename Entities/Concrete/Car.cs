ca468e10b55d6a851ac5cbab60ca9a683781b4b0
Batuhan Erol <bataer57@gmail.com>
2022-07-15 16:36:52 +0300

    Modified | Car && Package

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -14,6 +14,6 @@ namespace Entities.Concrete
         public int ColorId { get; set; }
         public int ModelYear { get; set; }
         public double DailyPrice { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; }
     }
 }
