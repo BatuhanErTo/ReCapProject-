ec9b102cebd113f274449673fec029fe51e270fc
Batuhan Erol <bataer57@gmail.com>
2022-07-15 16:38:10 +0300

    Added Entity Framework

---
+++ DataAccess/Concrete/EntityFramework/ReCapContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ReCapContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = (localdb)\mssqllocaldb;Database = ReCap;Trusted_Connection = true");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
