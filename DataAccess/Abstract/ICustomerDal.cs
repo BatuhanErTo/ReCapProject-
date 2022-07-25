aab509d5f13b87a1e7e9382e6f32fbe0b8e2585a
Batuhan Erol <bataer57@gmail.com>
2022-07-25 23:03:32 +0300

    New entities Dals added

---
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Core.DataAccess;
+
+namespace DataAccess.Abstract
+{
+    public interface ICustomerDal : IEntityRepository<Customer>
+    {
+    }
+}
---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+    }
+}
