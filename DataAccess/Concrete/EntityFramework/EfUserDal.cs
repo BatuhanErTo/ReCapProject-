ce8241b1bd0fd19bc341d5c8807bea2db3781a08
Batuhan Erol <bataer57@gmail.com>
2022-07-25 23:14:58 +0300

    CRUD operations of new entities done

--- DataAccess/Concrete/EntityFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    internal class EfUserDal
+    public class EfUserDal : EfEntityRepositoryBase<User,ReCapContext>, IUserDal
     {
     }
 }
