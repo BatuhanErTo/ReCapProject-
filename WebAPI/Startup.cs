8b84b89458d27905297c3d268ca1df833a38463a
Batuhan Erol <bataer57@gmail.com>
2022-07-28 23:55:59 +0300

    WebAPI modified : cars,rentals,users,color,brands added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -38,6 +38,16 @@ namespace WebAPI
             });
             services.AddSingleton<IBrandService, BrandManager>();
             services.AddSingleton<IBrandDal, EfBrandDal>();
+            services.AddSingleton<IColorService,ColorManager>();
+            services.AddSingleton<IColorDal, EfColorDal>();
+            services.AddSingleton<ICustomerService, CustomerManager>();
+            services.AddSingleton<ICustomerDal, EfCustomerDal>();
+            services.AddSingleton<IUserService, UserManager>();
+            services.AddSingleton<IUserDal, EfUserDal>();
+            services.AddSingleton<IRentalService, RentalManager>();
+            services.AddSingleton<IRentalDal, EfRentalDal>();
+            services.AddSingleton<ICarService, CarManager>();
+            services.AddSingleton<ICarDal, EfCarDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
